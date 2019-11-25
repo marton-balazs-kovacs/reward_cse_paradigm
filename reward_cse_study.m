374b3575bf35f1e9fda4b03a115547efdaa4194b
Marcian <marton.balazs.kovacs@gmail.com>
2019-11-25 09:46:56 +0100

    Init

---
+++ reward_cse_study.m
@@ -0,0 +1,11 @@
+% Study paradigm used in the Steenbergen, Band, & Hommel (2009) paper
+
+% Instructions
+% Practice trials (n = 24)
+% Instructions
+% First test block (n = 204)
+% Break screen (max 40 sec)
+% Second test block (n = 204)
+% Break screen (max 40 sec)
+% Third test block (n = 204)
+% Filler block (n = 24)
